b7a27e0575c2c9fb79ec1a34a84f87be0c2a8a25
Kantner, Jakub <jakub.kantner@merck.com>
2021-06-05 11:23:40 +0200

    embedding

---
+++ unbinvectors.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  5 10:01:28 2021
+
+@author: kantnerj
+"""
+
+
+import gensim; 
+from gensim.models  import KeyedVectors; 
+location = "../GoogleNews-vectors-negative300.bin"
+wv = KeyedVectors.load_word2vec_format(location, binary=True, limit=1000000) ;
+wv.save_word2vec_format('vectors.csv')
